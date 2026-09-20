72ba9dbb5623babd44087d4b7f2e542c22d29847
ahmm123 <ambrnamhnwys@gmail.com>
2026-09-20 17:03:56 +0430

    created a simple lexer

--- src/lexer/lexer.cpp
+++ src/lexer/lexer.cpp
@@ -4,5 +4,26 @@
 #include <iostream>
 
 std::vector<std::string> tokenize(std::string content){
-  std::cout << content << '\n';
+  std::vector<std::string> tokens {};
+  std::string currentToken {};
+  char specialCahr {};
+  for (size_t i = 0; i < content.length(); i++) {
+    char currentChar = content[i];
+    if (currentChar == ' ' || currentChar == '\n') {
+      if (currentToken != "") {
+        tokens.push_back(currentToken);
+        currentToken = "";
+      }
+      continue;
+    }
+    currentToken.push_back(currentChar);
+    if (i == content.length()-1) {
+      tokens.push_back(currentToken);
+    }
+  }
+  for (size_t i = 0; i < tokens.size(); i++) {
+    std::cout << tokens[i] << '\n';
+  }
+  
+  return tokens;
 };
